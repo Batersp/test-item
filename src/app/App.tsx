af2090e4f0b620b2984e9795418135a09a101f2c
pavel_ishmukov <p-pup1@mail.ru>
2022-10-18 15:38:33 +0300

    change app tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,5 +3,10 @@ import React from 'react';
 import { ReturnComponentType } from 'common/types/ReturnComponentType';
 
 export const App = (): ReturnComponentType => {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>Hello guys</h1>
+      <div>I am fine</div>
+    </div>
+  );
 };
