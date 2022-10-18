c4465a6f8d48cdb44cbf990532afc56f229d63cf
pavel_ishmukov <p-pup1@mail.ru>
2022-10-18 15:33:27 +0300

    init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { ReturnComponentType } from 'common/types/ReturnComponentType';
+
+export const App = (): ReturnComponentType => {
+  return <div className="App" />;
+};
