7e23c0c9f3a9663a7e29252e30fe7509b0476b8e
pavel_ishmukov <p-pup1@mail.ru>
2022-10-23 19:35:31 +0300

    add page not found

--- src/features/Error404/Error404.tsx
+++ src/features/Error404/Error404.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 import style from './Error404.module.css';
 
+import image from 'assets/images/error404.png';
 import { ReturnComponentType } from 'common/types/ReturnComponentType';
 
-export const Error404 = (): ReturnComponentType => {
-  return (
-    <div className={style.container}>
-      <h1>Error404</h1>
-      <p>yes</p>
-    </div>
-  );
-};
+export const Error404 = (): ReturnComponentType => (
+  <div className={style.onStyle}>
+    <img src={image} alt="Ошибка 404" />
+  </div>
+);
